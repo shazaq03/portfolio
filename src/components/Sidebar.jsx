561d459172c3eed6fe05a042221ce2e5442fda2c
shazaq03 <shazaq03@gmail.com>
2022-12-16 00:28:23 +0530

    before final touch-ups- need to add resume link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,13 +11,13 @@ function Sidebar() {
             <Link to={'/contact'}><HiOutlineMail color='#333' fontSize="100%" /><span>Email</span></Link>
         </li>
         <li>
-            <a href="#" rel="noreferrer"><FiUser color='#333' fontSize="100%" /><span>Resume</span></a>
+            <a href="/" rel="noreferrer"><FiUser color='#333' fontSize="100%" /><span>Resume</span></a>
         </li>
         <li>
             <a href="https://github.com/shazaq03" target="_blank" rel="noreferrer"><FiGithub color='#333' fontSize="100%" /><span>Github</span></a>
         </li>
         <li>
-            <a href="#" rel="noreferrer"><FiLinkedin color='#333' fontSize="100%" /><span>LinkedIn</span></a>
+            <a href="https://www.linkedin.com/in/sharan-sampath-kumar-10b1a5208?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2F0%2FkZ6esTpyLw6CV3kWanA%3D%3D" target="_blank" rel="noreferrer"><FiLinkedin color='#333' fontSize="100%" /><span>LinkedIn</span></a>
         </li>
         <li>
             <a href="https://twitter.com/Shazaq03" target="_blank" rel="noreferrer"><FiTwitter color='#333' fontSize="100%" /><span>Twitter</span></a>
