31a0b555efedea943cc29811cf0ba0e26d662c94
shazaq03 <shazaq03@gmail.com>
2023-02-15 07:55:14 +0530

    updated new projects and resume

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,54 +1,42 @@
-
- export const ProjectData = [
-
-    {
-        index: 1,
-        name: "Redux - Contact App",
-        image: "/images/contact-app-project-thumb.PNG",
-        glink: "https://github.com/shazaq03/react-redux-contacts-app.git",
-        Llink: "https://shazaq03.github.io/react-redux-contacts-app/",
-        description: "",
-
-
-    },
-    {
-        index: 2,
-        name: "React - Recipe App",
-        image: "/images/recipe-app-project-thumb.PNG",
-        glink: "https://github.com/shazaq03/react-recipe-app.git",
-        Llink: "https://shazaq03.github.io/react-recipe-app/",
-        description: "",
-
-
-    },
-    {
-        index: 3,
-        name: "React - Calculator",
-        image: "/images/calculator-project-thumb.png",
-        glink: "https://github.com/shazaq03/calculator",
-        Llink: "https://shazaq03.github.io/calculator/",
-        description: "",
-
-
-    },
-    {
-        index: 4,
-        name: "React - Todo list",
-        image: "/images/todo-app-project-thumb.PNG",
-        glink: "https://github.com/shazaq03/react-todo-list",
-        Llink: "https://shazaq03.github.io/react-todo-list/",
-        description: "",
-
-
-    },
-    {
-        index: 5,
-        name: "Js - Classic snake game",
-        image: "/images/snake-game-project-thumb.PNG",
-        glink: "https://github.com/shazaq03/snake-game",
-        Llink: "https://shazaq03.github.io/snake-game/",
-        description: "",
-
-
-    },
+export const ProjectData = [
+	{
+		index: 1,
+		name: "Redux - Contact App",
+		image: "/images/contact-app-project-thumb.PNG",
+		glink: "https://github.com/shazaq03/react-redux-contacts-app.git",
+		Llink: "https://shazaq03.github.io/react-redux-contacts-app/",
+		description: "",
+	},
+	{
+		index: 2,
+		name: "Paimon's Handbook",
+		image: "/images/paimons-handbook-project-thumb.png",
+		glink: "https://github.com/shazaq03/paimons-handbook",
+		Llink: "https://paimons-handbook.netlify.app/",
+		description: "",
+	},
+	{
+		index: 3,
+		name: "React - Recipe App",
+		image: "/images/recipe-app-project-thumb.PNG",
+		glink: "https://github.com/shazaq03/react-recipe-app.git",
+		Llink: "https://shazaq03.github.io/react-recipe-app/",
+		description: "",
+	},
+	{
+		index: 4,
+		name: "React - Todo list",
+		image: "/images/todo-app-project-thumb.PNG",
+		glink: "https://github.com/shazaq03/react-todo-list",
+		Llink: "https://shazaq03.github.io/react-todo-list/",
+		description: "",
+	},
+	{
+		index: 5,
+		name: "Js - Classic snake game",
+		image: "/images/snake-game-project-thumb.PNG",
+		glink: "https://github.com/shazaq03/snake-game",
+		Llink: "https://shazaq03.github.io/snake-game/",
+		description: "",
+	},
 ];
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,59 @@
-import React from 'react';
-import {FiGithub, FiLinkedin, FiTwitter, FiUser} from 'react-icons/fi';
-import {HiOutlineMail} from  'react-icons/hi'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { FiGithub, FiLinkedin, FiTwitter, FiUser } from "react-icons/fi";
+import { HiOutlineMail } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
-  return (
-
-    <ul id="social-sidebar">
-        <li>
-            <Link to={'/contact'}><HiOutlineMail color='#333' fontSize="100%" /><span>Email</span></Link>
-        </li>
-        <li>
-            <a href="https://drive.google.com/file/d/1hJPKNcmS26Y339pVF8ihnOveh5KYmmW6/view?usp=share_link" target="_blank" rel="noreferrer"><FiUser color='#333' fontSize="100%" /><span>Resume</span></a>
-        </li>
-        <li>
-            <a href="https://github.com/shazaq03" target="_blank" rel="noreferrer"><FiGithub color='#333' fontSize="100%" /><span>Github</span></a>
-        </li>
-        <li>
-            <a href="https://www.linkedin.com/in/sharan-sampath-kumar-10b1a5208?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2F0%2FkZ6esTpyLw6CV3kWanA%3D%3D" target="_blank" rel="noreferrer"><FiLinkedin color='#333' fontSize="100%" /><span>LinkedIn</span></a>
-        </li>
-        <li>
-            <a href="https://twitter.com/Shazaq03" target="_blank" rel="noreferrer"><FiTwitter color='#333' fontSize="100%" /><span>Twitter</span></a>
-        </li>
-    </ul>
-
-  )
+	return (
+		<ul id="social-sidebar">
+			<li>
+				<Link to={"/contact"}>
+					<HiOutlineMail color="#333" fontSize="100%" />
+					<span>Email</span>
+				</Link>
+			</li>
+			<li>
+				<a
+					href="https://drive.google.com/file/d/1fbdToPPOgxjzAqPToVL5fh6rOZK_hIyd/view?usp=sharing"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<FiUser color="#333" fontSize="100%" />
+					<span>Resume</span>
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://github.com/shazaq03"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<FiGithub color="#333" fontSize="100%" />
+					<span>Github</span>
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://www.linkedin.com/in/sharan-sampath-kumar-10b1a5208?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2F0%2FkZ6esTpyLw6CV3kWanA%3D%3D"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<FiLinkedin color="#333" fontSize="100%" />
+					<span>LinkedIn</span>
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://twitter.com/Shazaq03"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<FiTwitter color="#333" fontSize="100%" />
+					<span>Twitter</span>
+				</a>
+			</li>
+		</ul>
+	);
 }
 
-export default Sidebar
+export default Sidebar;
